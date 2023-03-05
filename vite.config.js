5b5266b4ff8cc65c9dddbaa791b4fc8f5835beae
jorge.mora <jmora2ec@gmail.com>
2023-03-04 22:14:24 -0500

    update local repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecomerce-product-page-app/',
+  base: '/ecommerce-product-page-app/',
   plugins: [react()],
 });
